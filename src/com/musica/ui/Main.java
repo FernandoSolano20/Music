dbbae5aba9da9f1deb29f107a91d15122e7f4bd9
fesolano <fesolano@outcoding.com>
2019-12-17 00:31:32 -0600

    File connection reader

--- src/com/musica/ui/Main.java
+++ src/com/musica/ui/Main.java
@@ -5,26 +5,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Controller controller = new Controller();
-        if(controller.isAdminOnDB()){
-            Parent page = FXMLLoader.load(getClass().getResource("views/login/login.fxml"));
-            Scene scene = new Scene(page);
-            primaryStage.setTitle("Iniciar Sesión");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+    public void start(Stage primaryStage){
+        try {
+            Controller controller = new Controller();
+            if(controller.isAdminOnDB()){
+                Parent page = FXMLLoader.load(getClass().getResource("views/login/login.fxml"));
+                Scene scene = new Scene(page);
+                primaryStage.setTitle("Iniciar Sesión");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            }
+            else {
+                Parent page = FXMLLoader.load(getClass().getResource("views/registro/Admin/Register.fxml"));
+                Scene scene = new Scene(page);
+                primaryStage.setTitle("Registro Administrador");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            }
         }
-        else {
-            Parent page = FXMLLoader.load(getClass().getResource("views/registro/Admin/Register.fxml"));
-            Scene scene = new Scene(page);
-            primaryStage.setTitle("Registro Administrador");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        catch (Exception e){
+            AlertHelper.showAlert(Alert.AlertType.ERROR, "Error", e.getMessage());
         }
     }
 
